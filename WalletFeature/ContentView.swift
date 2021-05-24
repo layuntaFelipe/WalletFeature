0b2e40a9a5eb38bf9247c357c1087fb374eebe4a
Layuntafelipe <flobo0701@icloud.com>
2021-05-24 18:47:40 -0300

    Initial Commit

---
+++ WalletFeature/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalletFeature
+//
+//  Created by Felipe Lobo on 24/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WalletFeature/WalletFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletFeatureApp.swift
+//  WalletFeature
+//
+//  Created by Felipe Lobo on 24/05/21.
+//
+
+import SwiftUI
+
+@main
+struct WalletFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
