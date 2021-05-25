84f38728932c443b482a538a0eba568d6fd68920
Layuntafelipe <flobo0701@icloud.com>
2021-05-24 23:41:08 -0300

    Final Wallet Prototype

--- WalletFeature/App/ContentView.swift
+++ WalletFeature/App/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        WalletView(cardsNumber: cards.count, cards: cards, show: false)
-            .animation(.easeIn)
+        NavigationView {
+            WalletView(cardsNumber: cards.count, cards: cards, show: false)
+                .animation(.easeIn)
+                .navigationBarTitle("My Wallet")
+        }
     }
 }
 
--- WalletFeature/View/WalletView.swift
+++ WalletFeature/View/WalletView.swift
@@ -13,22 +13,30 @@ struct WalletView: View {
     @State var cardsNumber: Int
     @State var cards: [Card]
     @State var show: Bool
+    @State private var cardTouched: Bool = false
     //MARK: - BODY
     var body: some View {
         VStack(spacing: distance){
             ForEach(0..<cardsNumber) {number in
-                Button(action: {
-                    print("HI im the card: \(number)")
-                }, label: {
-                    ZStack{
-                        Color(.brown)
-                            .frame(width: 325, height: 190)
-                            .cornerRadius(20)
+                ZStack{
+                    Color(.brown)
+                        .frame(width: 325, height: 190)
+                        .cornerRadius(20)
+                    Button(action: {
+                        print("HI im the card: \(number)")
+                        cardTouched.toggle()
+                        if cardTouched {
+                            cards[number].cardOffset = -60
+                        } else {
+                            cards[number].cardOffset = 0
+                        }
+                    }, label: {
                         CardView(balance: cards[number].cardBalance, name: cards[number].cardName, firstColor: cards[number].cardFirstColor, secondColor: cards[number].cardSecondColor, offSet: cards[number].cardOffset)
                             .frame(width: 300, height: 186)
                             .shadow(color: Color(.black).opacity(0.5), radius: 5, x: 0.0, y: -5)
-                    }//ZSTACK
-                })//BUTTON
+                    })//BUTTON
+                    .offset(y: cards[number].cardOffset)
+                }//ZSTACK
             }//LOOP
             ZStack{
                 Color(.brown)
@@ -41,7 +49,7 @@ struct WalletView: View {
                     if show{
                         distance = -130
                     } else {
-                        distance = -18
+                        distance = -180
                     }
                 }, label: {
                     if show{
